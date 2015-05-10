3408be160a01b425a1d47381365431ce39f35016
already-dead <seva1796@gmail.com>
2015-05-10 14:18:19 +0300

    adddddd

--- web/src/main/java/com/Command.java
+++
@@ -1,9 +0,0 @@
-package com;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-public abstract class Command {
-	abstract public void execute(HttpServletRequest request,HttpServletResponse response);
-
-}
