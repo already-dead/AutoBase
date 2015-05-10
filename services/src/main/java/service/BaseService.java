bb5bab9823e9ee04cdd408be5cb9b24c8b8fe7c8
already-dead <seva1796@gmail.com>
2015-05-10 13:17:49 +0300

    dell all 2

--- services/src/main/java/service/BaseService.java
+++
@@ -1,8 +0,0 @@
-package service;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-public abstract class BaseService {
-	abstract public void serv(HttpServletRequest request,HttpServletResponse response);
-}
